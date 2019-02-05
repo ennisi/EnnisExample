3ec8c2597f283cbeab81ddb09332d1678fbaee71
Ennis_W <1653504352@qq.com>
2019-02-05 17:33:07 +0800

    Add files via upload

---
+++ STM.m
@@ -0,0 +1,14 @@
+%传统中值滤波plus开关判断
+Image=imread('lena.jpg');
+NoiseImage=imnoise(Image,'salt & pepper',0.8);
+[sizex,sizey]=size(NoiseImage);
+
+for i=2:sizex-1
+    for j=2:sizey-1
+        Temp=[NoiseImage(i-1,j-1),NoiseImage(i-1,j),NoiseImage(i-1,j+1),NoiseImage(i,j-1),NoiseImage(i,j),NoiseImage(i,j+1),NoiseImage(i+1,j-1),NoiseImage(i+1,j),NoiseImage(i+1,j+1)];
+        M=median(Temp);
+        if NoiseImage(i,j)==0||NoiseImage(i,j)==255
+            NoiseImage(i,j)=M;
+        end
+    end
+end
